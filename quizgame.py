edfab66b171ad7b6b43af0a45a63ca22bcf59f8c
Mina Lamit <95231683+carminalamit@users.noreply.github.com>
2022-12-13 09:45:32 +0800

    Add files via upload

---
+++ quizgame.py
@@ -0,0 +1,106 @@
+print('''\033[1;33m⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⣀⣀⣀⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⡠⠤⠄⠄⠄⣒⣒⣮⣥⣬⣥⣴⣶⣾⡿⣦⣌⠡⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⣼⡷⢺⡿⠿⠿⢿⡟⢹⡿⠉⡏⠄⣧⣴⡆⢠⣿⣿⡇⠇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⣿⣿⣾⠇⢰⡆⢸⡇⢸⡇⢠⡇⢠⣿⡏⢠⣾⣭⣷⡆⡆⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⣿⣿⣿⠄⢸⡇⢸⡇⢸⡇⢸⡇⢸⡏⠠⠟⢻⣿⣿⡇⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢿⣿⣿⠄⠛⠃⢸⣇⣈⣡⣼⣧⣼⣷⡶⠶⢿⠛⣿⡇⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⣿⣿⠿⠿⠛⠋⢹⡏⠉⣿⠁⢸⡇⢰⣶⡇⢀⣿⡇⠇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⣿⣿⣶⠄⣿⡇⢸⡇⠄⠉⠄⢸⡇⢈⣹⡇⢸⣿⡇⡂⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢹⣽⣿⠄⣿⡇⢸⡇⠄⠄⡄⢸⡇⠘⠛⡏⣹⣿⡇⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⣿⣿⠄⡟⣇⣸⣧⣴⣤⣷⣾⣿⣿⣿⣿⣿⣿⡟⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢿⣿⠛⢿⣟⣹⣿⣿⣿⣿⣿⣿⣿⡿⠟⠉⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠸⠇⠄⠄⢹⣯⣹⠿⠟⢋⣿⡭⠉⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⣤⡄⠚⠛⠋⠁⠄⠄⠄⡸⠋⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⠙⠛⠂⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+\033[0m''')
+
+# Ask user name
+name = input("Enter your Name: ")
+print(f"\033[1;32m\nGreetings {name}!🎈 Welcome To Software Development Quiz Game!💻 \033[0m")
+
+play = input("\nAre you ready to play the Quiz? ")
+
+if play.lower() == "yes":
+    print("\nAwesome! Let's play 😈")
+else:
+    print("\nOkay! See you next time...")
+
+score = 0
+
+# Questions for the game
+answer = input("\nQuestion 1: A technique used to describe step by step process and can help a programmer outline their code.\nType in your answer: ")
+if answer.lower() == "pseudocode":
+    print("\033[1;32m\nNice!🥳 You got it.\033[0m")
+    score += 1
+else:
+    print("\033[1;32m\nIncorrect!😞 The answer is Pseudocode\033[0m")
+
+answer = input("\nQuestion 2: Just as buildings and houses have a street  address, webpages also have unique addresses to help people locate them. On the internet, these addresses are called _________?\nType in your answer: ")
+if answer.lower() == "url":
+    print("\033[1;32m\nCorrect!🥳\033[0m")
+    score += 1
+else:
+    print("\033[1;32m\nWrong!😞 The correct answer is URL (Uniform Resource Locators)\033[0m")
+
+answer = input("\nQuestion 3: A mathematical function used to perform calculations like addition, subtraction, multiplication, etc.\nType in your answer: ")
+if answer.lower() == "arithmetic":
+    print("\033[1;32m\nThat's right!🥳\033[0m")
+    score += 1
+else:
+    print("\033[1;32m\nIncorrect!😞 The answer is Arithmetic\033[0m")
+
+answer = input("\nQuestion 4: What is an open-source version control system that tracks changes in any set of files, usually used for coordinating work among programmers collaboratively.\nType in your answer: ")
+if answer.lower() == "git":
+    print("\033[1;32m\nGreat!🥳 Your right.\033[0m")
+    score += 1
+else:
+    print("\033[1;32m\nWrong!😞 The correct answer is Git\033[0m")
+
+answer = input("\nQuestion 5: A standard markup language for creating and describes the structure of a Web page.\nType in your answer: ")
+if answer.lower() == "html":
+    print("\033[1;32m\nCorrect!🥳\033[0m")
+    score += 1
+else:
+    print("\033[1;32m\nIncorrect!😞 The answer is HTML (Hyper Text Markup Language)\033[0m")
+
+answer = input("\nQuestion 6: What is an application protocol for distributed, collaborative, hypermedia information systems that allows users to communicate data on the World Wide Web?\nType in your answer: ")
+if answer.lower() == "http":
+    print("\033[1;32m\nNice!🥳 You got it.\033[0m")
+    score += 1
+else: 
+    print("\033[1;32m\nWrong!😞 The correct answer is HTTP (Hypertext Transfer Protocol)\033[0m")
+
+answer = input("\nQuestion 7: It is used to style and layout web pages, for example: to alter the font, color, size, and spacing of your content, split it into multiple columns, or add animations and other decorative features.\nType in your answer: ")
+if answer.lower() == "css":
+    print("\033[1;32m\nYour right!🥳\033[0m")
+    score += 1
+else:
+    print("\033[1;32m\nIncorrect!😞 The answer is CSS (Cascading Style Sheets)\033[0m")
+
+answer = input("\nQuestion 8: A software intermediary that allows two applications to talk to each other. In other words, a messenger that delivers your request to the provider that you're requesting it from and then delivers the response back to you.\nType in your answer: ")
+if answer.lower() == "api":
+    print("\033[1;32m\nNice!🥳 You got it.\033[0m")
+    score += 1
+else:
+    print("\033[1;32m\nWrong!😞 The correct answer is API ( Application Programming Interface)\033[1;36m")
+
+answer = input("\nQuestion 9: It defines the step by step approach for the development of software and involves the following phases. In other words Requirement Gathering, System Analysis, Design, Coding, Testing, Maintenance, and Documentation.\nType in your answer: ")
+if answer.lower() == "sdlc" or answer.lower() == "software development life cycle":
+    print("\033[1;32m\nGreat!🥳 Your right.\033[0m")
+    score += 1
+else:
+    print("\033[1;32m\nIncorrect!😞 The answer is SDLC (Software Development Life Cycle)\033[0m")
+
+answer = input("\nQuestion 10: A online software development platform that developers used for storing, tracking, and collaborating on software projects.\nType in your answer: ")
+if answer.lower() == "github":
+    print("\033[1;32m\nCorrect!🥳\033[0m")
+    score += 1
+else:
+    print("\033[1;32m\nWrong!😞 The correct answer is GitHub\033[0m")
+
+# Printing result
+print(f"\033[33m\nYou got " + str(score) + " questions correct!👏👏")
+print(f"\nYour average score is " + str((score / 10) * 100) + "%.\n\nThank you for playing Software Development Quiz Game!😃\n")
